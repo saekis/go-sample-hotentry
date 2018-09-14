08c652192efaad55bf60766323de05a69ecf5623
saekis <s.saeki71@gmail.com>
2018-09-14 19:43:48 +0900

    fix compile error

--- client/hatena_client_test.go
+++ client/hatena_client_test.go
@@ -2,13 +2,16 @@ package client_test
 
 import (
 	"net/http"
+	"testing"
 
+	"github.com/saekis/go-sample-hotentry/client"
+	"github.com/saekis/go-sample-hotentry/config"
 	"github.com/saekis/go-sample-hotentry/model/hatena"
 )
 
 type mockParser struct{}
 
-func (mockParser) parseToEntryList(*http.Response, int) (*hatena.EntryList, error) {
+func (mockParser) ParseToEntryList(*http.Response, int) (*hatena.EntryList, error) {
 	return &hatena.EntryList{
 		hatena.Entry{},
 		hatena.Entry{},
@@ -22,15 +25,14 @@ func (mockHTTPClient) Get(url string) (*http.Response, error) {
 	return &http.Response{}, nil
 }
 
-// mockParser{}の初期化でまだエラーになる...
-// func TestGetHotentryList_Success(t *testing.T) {
-// 	mockhatenaclient, err := client.NewHatenaClient(mockHTTPClient{}, &config.Hatena{}, mockParser{})
-// 	if err != nil {
-// 		t.Errorf("got: %v\nwant: %v", err, nil)
-// 	}
+func TestGetHotentryList_Success(t *testing.T) {
+	mockhatenaclient, err := client.NewHatenaClient(mockHTTPClient{}, &config.Hatena{}, mockParser{})
+	if err != nil {
+		t.Errorf("got: %v\nwant: %v", err, nil)
+	}
 
-// 	_, err = mockhatenaclient.GetHotentryList()
-// 	if err != nil {
-// 		t.Errorf("got: %v\nwant: %v", err, nil)
-// 	}
-// }
+	_, err = mockhatenaclient.GetHotentryList()
+	if err != nil {
+		t.Errorf("got: %v\nwant: %v", err, nil)
+	}
+}
